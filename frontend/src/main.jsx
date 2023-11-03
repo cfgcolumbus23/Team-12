6c9e9cc8a22b4b4a04cae3377b538a7fec0dd520
Tyler <tylermauch18@gmail.com>
2023-11-03 16:28:53 -0400

    added router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<App/>}></Route>
+    </Routes>
+  </BrowserRouter>
 )
