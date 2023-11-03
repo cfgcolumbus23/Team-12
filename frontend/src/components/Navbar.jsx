1102e5a9af64ea55e6cc4b5d1eaf51a2815ec64e
Tyler <tylermauch18@gmail.com>
2023-11-03 18:54:27 -0400

    working navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { Link, useHistory  } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 const Navbar = () => {
     //const history = useHistory(); // Initialize the useHistory hook
