f820ceb84f87e34290f2ebf135ccd77aa72cf246
alexa614 <alexa.yockey@gmail.com>
2023-11-04 06:50:22 -0400

    added nav bars to pages

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './login.css';
 import axios from "axios";
+import Navbar from "./Navbar.jsx";
 import {useNavigate} from 'react-router-dom';
 
 const SignIn = () => {
@@ -32,6 +33,7 @@ const SignIn = () => {
     }
 
     return <div className="form" onSubmit={loginUser}>
+        <Navbar />
         <div className="form-input-sections">
             <div className="id">
                   <label className="form-label" for="id">Employee ID</label>
@@ -47,3 +49,93 @@ const SignIn = () => {
 }
 
 export default SignIn
+
+
+
+// import React, { useState } from "react";
+// import "./login.css";
+// import Navbar from "./Navbar.jsx";
+// import { Link } from "react-router-dom";
+
+// const SignIn = () => {
+//   const [employeeID, setEmployeeID] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+
+//   const isNumeric = (str) => {
+//     // Use a regular expression to check if the string contains only numeric characters
+//     return /^\d+$/.test(str);
+//   };
+
+//   const handleSignIn = () => {
+//     setIsFormValid(true);
+    
+//     if (employeeID.trim() === "") {
+//       setError("Employee ID is required");
+//       setIsFormValid(false);
+//       return;
+//     }
+
+//     if (password.trim() === "") {
+//       setError("Password is required");
+//       setIsFormValid(false);
+//       return;
+//     }
+
+//     if (!isNumeric(employeeID)) {
+//       setError("Employee ID must contain only numbers");
+//       setIsFormValid(false);
+//       return;
+//     }
+
+//     // If validation passes, you can proceed with the sign-in logic here
+//     // For now, we'll just show an alert with the input values
+//     // alert(`Employee ID: ${employeeID}\nPassword: ${password}`);
+//   };
+
+//   return (
+//     <div className="form">
+//       <Navbar />
+//       <div className="form-input-sections">
+//         <h2> Login</h2>
+//         <h3> Welcome Back!</h3>
+//         <div className="id">
+//           <label className="form-label" htmlFor="id">
+//             Employee ID
+//           </label>
+//           <input
+//             className="form-input"
+//             type="text"
+//             id="id"
+//             placeholder="Employee ID"
+//             value={employeeID}
+//             onChange={(e) => setEmployeeID(e.target.value)}
+//           />
+//         </div>
+//         <div className="password">
+//           <label className="form-label" htmlFor="password">
+//             Password
+//           </label>
+//           <input
+//             className="form-input"
+//             type="password" // Use type="password" for password input
+//             id="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </div>
+//       </div>
+//       {error && <div className="error-message">{error}</div>}
+//       <div className="buttonDiv">
+//         <Link to="/profile">
+//           <button type="button" className="button" onClick={handleSignIn}>
+//             Sign In
+//           </button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignIn;
