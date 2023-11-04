37d72bd1a34da152e9f95459dda03a1059da0795
Mithra Sankar <mithra.sankar@gmail.com>
2023-11-03 20:55:33 -0400

    Form validation fix

--- frontend/src/feedback.jsx
+++ frontend/src/feedback.jsx
@@ -3,15 +3,35 @@ import "./feedback.css";
 
 const Feedback = () => {
   const [formData, setFormData] = useState({
+    //expected input
     preferredName: "",
     id: "",
     email: "",
     comments: "",
   });
+  const [errors, setErrors] = useState({
+    //for invalid input
+    preferredName: "",
+    id: "",
+    email: "",
+  });
 
   const handleInput = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value }); //  copy w/ value updated
+    //check for valid input
+    const rules = {
+      preferredName: /^[A-Za-z\s]+$/,
+      id: /^[0-9]+$/,
+      email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    };
+    const is_Valid = rules[name].test(value);
+    setErrors({
+      ...errors,
+      [name]: is_Valid ? "" : "Please enter valid input",
+    });
+    if (is_Valid) {
+      setFormData({ ...formData, [name]: value }); //  copy w/ value updated
+    }
   };
 
   const handleSubmit = (e) => {
@@ -20,6 +40,7 @@ const Feedback = () => {
 
   return (
     <div className="form">
+      <h2> Contact Us </h2>
       <div className="form-input-sections">
         <div className="preferredName">
           <label className="form-label" htmlFor="preferredName">
@@ -35,6 +56,7 @@ const Feedback = () => {
             placeholder="Preferred Name"
             required
           />
+          <div className="error">{errors.preferredName}</div>
         </div>
         <div className="id">
           <label className="form-label" htmlFor="id">
@@ -50,6 +72,7 @@ const Feedback = () => {
             placeholder="Employee ID"
             required
           />
+          <div className="error">{errors.id}</div>
         </div>
         <div className="email">
           <label className="form-label" htmlFor="email">
@@ -65,6 +88,7 @@ const Feedback = () => {
             placeholder="Email"
             required
           />
+          <div className="error">{errors.email}</div>
         </div>
         <div className="comment">
           <label className="form-label" htmlFor="Comments">
