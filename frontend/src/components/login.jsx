abaf84e8823bdea1c99a3c2f7ccdeadd8f8e7cbd
alexa614 <alexa.yockey@gmail.com>
2023-11-04 08:59:37 -0400

    comments

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -32,6 +32,7 @@ const SignIn = () => {
         }
     }
 
+    // make form with text boxes for users to enter their ID and password
     return <div className="form" onSubmit={loginUser}>
         <Navbar />
         <div className="form-input-sections">
@@ -50,93 +51,3 @@ const SignIn = () => {
 }
 
 export default SignIn
-
-
-
-// import React, { useState } from "react";
-// import "./login.css";
-// import Navbar from "./Navbar.jsx";
-// import { Link } from "react-router-dom";
-
-// const SignIn = () => {
-//   const [employeeID, setEmployeeID] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState("");
-
-//   const isNumeric = (str) => {
-//     // Use a regular expression to check if the string contains only numeric characters
-//     return /^\d+$/.test(str);
-//   };
-
-//   const handleSignIn = () => {
-//     setIsFormValid(true);
-    
-//     if (employeeID.trim() === "") {
-//       setError("Employee ID is required");
-//       setIsFormValid(false);
-//       return;
-//     }
-
-//     if (password.trim() === "") {
-//       setError("Password is required");
-//       setIsFormValid(false);
-//       return;
-//     }
-
-//     if (!isNumeric(employeeID)) {
-//       setError("Employee ID must contain only numbers");
-//       setIsFormValid(false);
-//       return;
-//     }
-
-//     // If validation passes, you can proceed with the sign-in logic here
-//     // For now, we'll just show an alert with the input values
-//     // alert(`Employee ID: ${employeeID}\nPassword: ${password}`);
-//   };
-
-//   return (
-//     <div className="form">
-//       <Navbar />
-//       <div className="form-input-sections">
-//         <h2> Login</h2>
-//         <h3> Welcome Back!</h3>
-//         <div className="id">
-//           <label className="form-label" htmlFor="id">
-//             Employee ID
-//           </label>
-//           <input
-//             className="form-input"
-//             type="text"
-//             id="id"
-//             placeholder="Employee ID"
-//             value={employeeID}
-//             onChange={(e) => setEmployeeID(e.target.value)}
-//           />
-//         </div>
-//         <div className="password">
-//           <label className="form-label" htmlFor="password">
-//             Password
-//           </label>
-//           <input
-//             className="form-input"
-//             type="password" // Use type="password" for password input
-//             id="password"
-//             placeholder="Password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//         </div>
-//       </div>
-//       {error && <div className="error-message">{error}</div>}
-//       <div className="buttonDiv">
-//         <Link to="/profile">
-//           <button type="button" className="button" onClick={handleSignIn}>
-//             Sign In
-//           </button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignIn;
