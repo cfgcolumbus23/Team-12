35fa8d65b4cd49e8e621b5089f5061953e0bfcce
Tyler <tylermauch18@gmail.com>
2023-11-03 23:10:58 -0400

    added grid layout for training program categories

--- frontend/src/components/courses.jsx
+++ frontend/src/components/courses.jsx
@@ -5,10 +5,12 @@ const Training = () => {
   return (
     <div id = "coursesContainer">
         <div id = "coursesContentContainer">
-            <div className = "courseCategory" id = "firstCourseCategory">Basic Work</div>
-            <div className = "courseCategory" id = "secondCourseCategory"></div>
-            <div className = "courseCategory" id = "thirdCourseCategory"></div>
-            <div className = "courseCategory" id = "fourthCourseCategory"></div>
+            <div className = "courseCategory" id = "firstCourseCategory">Basic Work Readiness</div>
+            <div className = "courseCategory" id = "secondCourseCategory">Digital Literacy</div>
+            <div className = "courseCategory" id = "thirdCourseCategory">Information Technology</div>
+            <div className = "courseCategory" id = "fourthCourseCategory">Healthcare</div>
+            <div className = "courseCategory" id = "fourthCourseCategory">Hospitality</div>
+            <div className = "courseCategory" id = "fourthCourseCategory">Logistics</div>
         </div>
         <Navbar/>
     </div>
