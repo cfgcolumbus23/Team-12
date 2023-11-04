2892b0b54e91e9f39357debf66adcf16c0a7cbfb
AbdelKharij <kharij.abdel@gmail.com>
2023-11-04 01:05:11 -0400

    Popup component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Popup from './components/Popup'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Popup/>
     </>
   )
 }
