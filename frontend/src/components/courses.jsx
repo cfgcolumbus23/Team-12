ddfd075fab18771e2846715a3b087fd6f3da2757
Tyler <tylermauch18@gmail.com>
2023-11-03 23:46:27 -0400

    implemented adaptive popup for course category clicks

--- frontend/src/components/courses.jsx
+++ frontend/src/components/courses.jsx
@@ -4,7 +4,8 @@ import "./courses.css"
 import { useState } from 'react'
 const Training = () => {
     const [popUpOpen,setPopUpOpen] = useState(false)
-    function handleCategoryClick(category){
+    const [popUpCategory,setPopUpCategory] = useState("basic")
+    function showCategoryPopUp(category){
         // hashes that help display the proper data
         let categoryTitleHash = {
             "basic":"Basic Work Readiness",
@@ -27,23 +28,27 @@ const Training = () => {
             return(
                 <div id = "popUpBackground" onClick = {() => setPopUpOpen(false)}>
                     <div id = "popUpMain">
-                        <div id = "popUpHeader">HEADER</div>
+                        <div id = "popUpHeader">{categoryTitleHash[popUpCategory]}</div>
                     </div>
                 </div>
             )
         }
     }
+    function handleCategoryClick(category){
+        setPopUpOpen(true)
+        setPopUpCategory(category)
+    }
   return (
     <div id = "coursesContainer">
         <div id = "coursesContentContainer">
-            <div className = "courseCategory" id = "firstCourseCategory" onClick={()=>setPopUpOpen(true)}>Basic Work Readiness</div>
-            <div className = "courseCategory" id = "secondCourseCategory">Digital Literacy</div>
-            <div className = "courseCategory" id = "thirdCourseCategory">Information Technology</div>
-            <div className = "courseCategory" id = "fourthCourseCategory">Healthcare</div>
-            <div className = "courseCategory" id = "fourthCourseCategory">Hospitality</div>
-            <div className = "courseCategory" id = "fourthCourseCategory">Logistics</div>
+            <div className = "courseCategory" id = "firstCourseCategory" onClick={()=>handleCategoryClick("basic")}>Basic Work Readiness</div>
+            <div className = "courseCategory" id = "secondCourseCategory" onClick={()=>handleCategoryClick("digital")}>Digital Literacy</div>
+            <div className = "courseCategory" id = "thirdCourseCategory" onClick={()=>handleCategoryClick("information")}>Information Technology</div>
+            <div className = "courseCategory" id = "fourthCourseCategory" onClick={()=>handleCategoryClick("healthcare")}>Healthcare</div>
+            <div className = "courseCategory" id = "fourthCourseCategory" onClick={()=>handleCategoryClick("hospitality")}>Hospitality</div>
+            <div className = "courseCategory" id = "fourthCourseCategory" onClick={()=>handleCategoryClick("logistics")}>Logistics</div>
         </div>
-        {handleCategoryClick()}
+        {showCategoryPopUp(popUpCategory)}
         <Navbar/>
     </div>
   )
