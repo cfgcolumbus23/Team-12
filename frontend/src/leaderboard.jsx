842e2c38476d30b5adc47b53898fc75560ee7659
Mithra Sankar <mithra.sankar@gmail.com>
2023-11-03 23:56:20 -0400

    leaderboard

--- frontend/src/leaderboard.jsx
+++ frontend/src/leaderboard.jsx
@@ -1,97 +1,97 @@
-import React from 'react';
-import './leaderboard.css';
+import React, { useState } from "react";
+import "./leaderboard.css";
 
-const Leaderboard = [
-    {
-        name: "Shawn Hanna",
-        location: "India",
-        score : 1550,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-02-10"
-    },
-    {
-        name: "Fidel Hand",
-        location: "USA",
-        score : 2310,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-01-01"
-    },
-    {
-        name: "Bessie Hickle",
-        location: "Chaina",
-        score : 350,
-        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-08-17"
-    },
-    {
-        name: "Adella Wunsch",
-        location: "Japan",
-        score : 2100,
-        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-10-23"
-    },
-    {
-        name: "Clair O'Connell",
-        location: "London",
-        score : 1250,
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-22"
-    },
-    {
-        name: "Kameron Prosacco",
-        location: "Canada",
-        score : 5250,
-        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-21"
+const data = [
+  {
+    name: "Shawn Hanna",
+    location: "India",
+    score: 1550,
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-02-10",
+  },
+  {
+    name: "Fidel Hand",
+    location: "USA",
+    score: 2310,
+    img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2021-01-01",
+  },
+  {
+    name: "Bessie Hickle",
+    location: "Chaina",
+    score: 350,
+    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2021-08-17",
+  },
+  {
+    name: "Adella Wunsch",
+    location: "Japan",
+    score: 2100,
+    img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2021-10-23",
+  },
+  {
+    name: "Clair O'Connell",
+    location: "London",
+    score: 1250,
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-01-22",
+  },
+  {
+    name: "Kameron Prosacco",
+    location: "Canada",
+    score: 5250,
+    img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-01-21",
+  },
+];
+
+function between(data, between) {
+  const today = new Date();
+  const previous = new Date(today);
+  previous.setDate(previous.getDate() - (between + 1));
+
+  let filter = data.filter((val) => {
+    let userDate = new Date(val.dt);
+    if (between == 0) return val;
+    return previous <= userDate && today >= userDate;
+  });
+
+  // sort with asending order
+  return filter.sort((a, b) => {
+    if (a.score === b.score) {
+      return b.score - a.score;
+    } else {
+      return b.score - a.score;
     }
-]
+  });
+}
 
-export default function Board() {
-
-    const [period, setPeriod] = useState(0);
+const Leaderboard = () => {
+  const [period, setPeriod] = useState(0);
 
   const handleClick = (e) => {
-     
-    setPeriod(e.target.dataset.id)
-  }
+    setPeriod(e.target.dataset.id);
+  };
 
   return (
     <div className="board">
-        <h1 className='leaderboard'>Leaderboard</h1>
+      <h1 className="leaderboard">Leaderboard</h1>
 
-        <div className="duration">
-            <button onClick={handleClick} data-id='7'>7 Days</button>
-            <button onClick={handleClick} data-id='30'>30 Days</button>
-            <button onClick={handleClick} data-id='0'>All-Time</button>
-        </div>
+      <div className="duration">
+        <button onClick={handleClick} data-id="7">
+          7 Days
+        </button>
+        <button onClick={handleClick} data-id="30">
+          30 Days
+        </button>
+        <button onClick={handleClick} data-id="0">
+          All-Time
+        </button>
+      </div>
 
-        <Profiles Leaderboard={between(Leaderboard, period)}></Profiles>
-
+      <Profiles Leaderboard={between(data, period)}></Profiles>
     </div>
-  )
-}
-
-
-
-function between(data, between){
-    const today = new Date();
-    const previous = new Date(today);
-    previous.setDate(previous.getDate() - (between + 1));
-
-    let filter = data.filter(val => {
-        let userDate = new Date(val.dt);
-        if (between == 0) return val;
-        return previous <= userDate && today >= userDate;
-    })
-
-    // sort with asending order
-    return filter.sort((a, b) => {
-        if ( a.score === b.score){
-            return b.score - a.score;
-        } else{
-            return b.score - a.score;
-        }
-    })
-
-}
-
+  );
+};
+export default Leaderboard;
