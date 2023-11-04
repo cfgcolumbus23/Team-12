dcbc95d79b7aa49b9c297e30a5d69a7b3ad891e4
alexa614 <alexa.yockey@gmail.com>
2023-11-04 00:06:00 -0400

    added stores to leaderboard and made it work

--- frontend/src/leaderboard.jsx
+++ frontend/src/leaderboard.jsx
@@ -2,49 +2,49 @@ import React, { useState } from "react";
 import "./leaderboard.css";
 
 const data = [
-  {
-    name: "Shawn Hanna",
-    location: "India",
-    score: 1550,
-    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2022-02-10",
-  },
-  {
-    name: "Fidel Hand",
-    location: "USA",
-    score: 2310,
-    img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2021-01-01",
-  },
-  {
-    name: "Bessie Hickle",
-    location: "Chaina",
-    score: 350,
-    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2021-08-17",
-  },
-  {
-    name: "Adella Wunsch",
-    location: "Japan",
-    score: 2100,
-    img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2021-10-23",
-  },
-  {
-    name: "Clair O'Connell",
-    location: "London",
-    score: 1250,
-    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2022-01-22",
-  },
-  {
-    name: "Kameron Prosacco",
-    location: "Canada",
-    score: 5250,
-    img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2022-01-21",
-  },
-];
+    {
+        name: "2550 N High Street",
+        location: "India",
+        score : 1550,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-02-10"
+    },
+    {
+        name: "2675 Brice Rd",
+        location: "USA",
+        score : 2310,
+        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-01-01"
+    },
+    {
+        name: "52 Robinwood",
+        location: "Chaina",
+        score : 350,
+        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-08-17"
+    },
+    {
+        name: "2933 Morse Road",
+        location: "Japan",
+        score : 2100,
+        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-10-23"
+    },
+    {
+        name: "5130 N Hamilton",
+        location: "London",
+        score : 1250,
+        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-01-22"
+    },
+    {
+        name: "5526 Renner Rd",
+        location: "Canada",
+        score : 5250,
+        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-01-21"
+    }
+]
 
 function between(data, between) {
   const today = new Date();
@@ -88,6 +88,28 @@ const Leaderboard = () => {
         <button onClick={handleClick} data-id="0">
           All-Time
         </button>
+        <ul>
+        <>
+            {
+                data.map((value, index) => (
+                    <div className="flex" key={index}>
+                        <div className="item">
+                            <img src={value.img} alt="" />
+            
+                            <div className="info">
+                                <h3 className='name text-dark'>{value.name}</h3>    
+                                <span>{value.location}</span>
+                            </div>                
+                        </div>
+                        <div className="item">
+                            <span>{value.score}</span>
+                        </div>
+                    </div>
+                    )
+                )
+            }
+        </>
+      </ul>
       </div>
 {/* 
       <Profiles Leaderboard={between(data, period)}></Profiles> */}
